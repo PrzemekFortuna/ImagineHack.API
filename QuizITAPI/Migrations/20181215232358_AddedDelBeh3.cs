c8b64d11e719ca19282af48a390863c368d193b1
DooMLooRD <37479784+DooMLooRD@users.noreply.github.com>
2018-12-16 00:40:08 +0100

    Add migration

---
+++ QuizITAPI/Migrations/20181215232358_AddedDelBeh3.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuizITAPI.Migrations
+{
+    public partial class AddedDelBeh3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxUsersCount",
+                table: "Rooms",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Rooms",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxUsersCount",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Rooms");
+        }
+    }
+}
