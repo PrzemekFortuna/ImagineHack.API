ce88bec5e8b807e09f678e7551d0b5fc59e60882
210176 <210176@edu.p.lodz.pl>
2018-12-16 05:20:26 +0100

    Authenticate

--- QuizITAPI/Controllers/OCRController.cs
+++ QuizITAPI/Controllers/OCRController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using QuizITAPI.Services;
 
 namespace QuizITAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class OCRController : ControllerBase
