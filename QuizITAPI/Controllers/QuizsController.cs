ef3069a2dba382769dd50c22db76a8a4b1c9fd65
210176 <210176@edu.p.lodz.pl>
2018-12-16 01:12:01 +0100

    Authorize quizs

--- QuizITAPI/Controllers/QuizsController.cs
+++ QuizITAPI/Controllers/QuizsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,7 @@ using QuizITAPI.Services;
 
 namespace QuizITAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class QuizsController : ControllerBase
